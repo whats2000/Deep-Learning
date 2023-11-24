7b4f61ff799438fc995c9d2069ece55b68dabf4b
whats2000 <whats2000mc@gmail.com>
2023-11-25 02:00:22 +0800

    Add varifocal loss

--- PyTorch/Class/pytorch105/A5/yolo_loss.py
+++ PyTorch/Class/pytorch105/A5/yolo_loss.py
@@ -1,20 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
 
-class FocalLoss(nn.Module):
-    def __init__(self, gamma=2.0, alpha=None):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-
-    def forward(self, inputs, targets):
-        bce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction='none')
-        pt = torch.exp(-bce_loss)
-        f_loss = self.alpha * (1-pt)**self.gamma * bce_loss
-
-        return f_loss.mean()
 
 def compute_iou(box1, box2):
     """Compute the intersection over union of two set of boxes, each box is [x1,y1,x2,y2].
@@ -128,7 +115,6 @@ class YoloLoss(nn.Module):
 
         return best_ious, best_boxes
 
-
     def get_class_prediction_loss(self, classes_pred, classes_target, has_object_map):
         """
         Parameters:
@@ -201,7 +187,7 @@ class YoloLoss(nn.Module):
         ### CODE
         # your code here
         # Use mean squared error loss
-        contain_conf_loss = F.mse_loss(box_pred_conf, box_target_conf , reduction='sum')
+        contain_conf_loss = F.mse_loss(box_pred_conf, box_target_conf, reduction='sum')
 
         return contain_conf_loss
 
@@ -277,7 +263,183 @@ class YoloLoss(nn.Module):
         best_ious, best_boxes = self.find_best_iou_boxes(pred_boxes_list, target_boxes)
 
         # compute contain_object_loss
-        containing_obj_loss = self.get_contain_conf_loss(best_boxes[..., 4].unsqueeze(-1), torch.ones_like(best_ious)) * inv_n
+        containing_obj_loss = self.get_contain_conf_loss(best_boxes[..., 4].unsqueeze(-1),
+                                                         torch.ones_like(best_ious)) * inv_n
+
+        # compute regression loss
+        reg_loss = self.get_regression_loss(best_boxes[..., :4], target_boxes[..., :4]) * inv_n
+
+        # compute final loss
+        total_loss = cls_loss + self.l_noobj * no_obj_loss + self.l_coord * reg_loss + containing_obj_loss
+
+        # construct return loss_dict
+        loss_dict = {
+            "total_loss": total_loss,
+            "reg_loss": reg_loss,
+            "containing_obj_loss": containing_obj_loss,
+            "no_obj_loss": no_obj_loss,
+            "cls_loss": cls_loss
+        }
+        return loss_dict
+
+
+class YoloLossV2(YoloLoss):
+    def __init__(self, s, b, l_coord, l_noobj):
+        """
+        Yolo Loss V2 with Focal Loss and Varifocal Loss.
+
+        Args:
+            S: Grid size
+            B: Number of bounding boxes
+            l_coord: Coefficient for the coordinate loss
+            l_noobj: Coefficient for the no-object loss
+        """
+        super(YoloLossV2, self).__init__(s, b, l_coord, l_noobj)
+
+    @staticmethod
+    def varifocal_loss(pred_score, gt_score, label, alpha=0.75, gamma=2.0):
+        """
+        Computes Varifocal Loss.
+
+        Args:
+            pred_score: Predicted scores
+            gt_score: Ground truth scores
+            label: Labels
+            alpha: Alpha value for balancing
+            gamma: Gamma value for focusing
+
+        Returns:
+            Varifocal loss value
+        """
+        weight = alpha * pred_score.sigmoid().pow(gamma) * (1 - label) + gt_score * label
+        with torch.cuda.amp.autocast(enabled=False):
+            loss = (F.binary_cross_entropy_with_logits(pred_score.float(), gt_score.float(), reduction='none') *
+                    weight).mean(1).sum()
+        return loss
+
+    @staticmethod
+    def focal_loss(pred, label, gamma=1.5, alpha=0.25):
+        """
+        Computes Focal Loss.
+
+        Args:
+            pred: Predicted logits
+            label: Ground truth labels
+            gamma: Gamma value for focusing
+            alpha: Alpha value for balancing
+
+        Returns:
+            Focal loss value
+        """
+        loss = F.binary_cross_entropy_with_logits(pred, label, reduction='none')
+        pred_prob = pred.sigmoid()
+        p_t = label * pred_prob + (1 - label) * (1 - pred_prob)
+        modulating_factor = (1.0 - p_t) ** gamma
+        loss *= modulating_factor
+        if alpha > 0:
+            alpha_factor = label * alpha + (1 - label) * (1 - alpha)
+            loss *= alpha_factor
+        return loss.mean(1).sum()
+
+    def get_class_prediction_loss(self, classes_pred, classes_target, has_object_map):
+        """
+        Calculate the class prediction loss using Varifocal Loss.
+
+        Args:
+            classes_pred: tensor of size (batch_size, S, S, 20), predicted class probabilities.
+            classes_target: tensor of size (batch_size, S, S, 20), ground truth for class presence.
+            has_object_map: tensor of size (batch_size, S, S), indicating which cells contain objects.
+
+        Returns:
+            class_loss: scalar, the total class prediction loss calculated using Varifocal Loss.
+
+        This method calculates the loss for the class predictions in cells that contain objects.
+        It uses Varifocal Loss to focus more on misclassified examples and address class imbalance.
+        """
+        object_mask = has_object_map.unsqueeze(-1).expand_as(classes_pred)
+        classes_pred = classes_pred[object_mask].view(-1, 20)  # Predicted class scores
+        classes_target = classes_target[object_mask].view(-1, 20)  # Ground truth class scores
+
+        # Ground truth scores for object classes (assuming binary classification for each class)
+        gt_scores = torch.ones_like(classes_target)
+
+        # Apply Varifocal Loss
+        class_loss = self.varifocal_loss(classes_pred, gt_scores, classes_target).sum()
+
+        return class_loss
+
+    def get_no_object_loss(self, pred_boxes_list, has_object_map):
+        """
+        Calculate the no-object loss using Varifocal Loss.
+
+        Args:
+            pred_boxes_list: list of tensors, each tensor is of size (Batch, S, S, 5), representing B predicted bounding boxes.
+            has_object_map: tensor of size (Batch, S, S), a boolean mask indicating cells containing objects.
+
+        Returns:
+            no_obj_loss: scalar, the computed no-object loss.
+
+        This function computes the Varifocal Loss for the cells that do not contain any objects. The loss is calculated
+        on the confidence score predictions of these cells, comparing them against a ground truth score of 0.
+        """
+        not_object_mask = ~has_object_map
+
+        repeated_not_object_mask = not_object_mask.unsqueeze(-1).repeat(1, 1, 1, self.B).reshape(-1)
+        flat_pred_conf = torch.cat([pred_boxes[..., 4].reshape(-1) for pred_boxes in pred_boxes_list], dim=0)
+        pred_conf_no_obj = flat_pred_conf[repeated_not_object_mask]
+
+        # Ground truth scores for non-object cells are 0
+        gt_conf_no_obj = torch.zeros_like(pred_conf_no_obj)
+
+        # Applying Varifocal Loss
+        no_obj_loss = self.varifocal_loss(pred_conf_no_obj, gt_conf_no_obj, torch.zeros_like(gt_conf_no_obj))
+
+        return no_obj_loss
+
+    def forward(self, pred_tensor, target_boxes, target_cls, has_object_map):
+        """
+        pred_tensor: (tensor) size(N,S,S,Bx5+20=30) N:batch_size
+                      where B - number of bounding boxes this grid cell is a part of = 2
+                            5 - number of bounding box values corresponding to [x, y, w, h, c]
+                                where x - x_coord, y - y_coord, w - width, h - height, c - confidence of having an object
+                            20 - number of classes
+
+        target_boxes: (tensor) size (N, S, S, 4): the ground truth bounding boxes
+        target_cls: (tensor) size (N, S, S, 20): the ground truth class
+        has_object_map: (tensor, bool) size (N, S, S): the ground truth for whether each cell contains an object (True/False)
+
+        Returns:
+        loss_dict (dict): with key value stored for total_loss, reg_loss, containing_obj_loss, no_obj_loss and cls_loss
+        """
+        N = pred_tensor.size(0)
+        inv_n = 1.0 / N
+        # When you calculate the classification loss, no-object loss, regression loss, contain_object_loss
+        # you need to multiply the loss with inv_n. e.g: inv_n * self.get_regression_loss(...)
+
+        # split the pred tensor from an entity to separate tensors:
+        # -- pred_boxes_list: a list containing all bbox prediction (list) [(tensor) size (N, S, S, 5)  for B pred_boxes]
+        # -- pred_cls (containing all classification predictions)
+        pred_boxes_list = [pred_tensor[:, :, :, i * 5:(i + 1) * 5] for i in range(self.B)]
+        pred_cls = pred_tensor[:, :, :, self.B * 5:]  # (N, S, S, 20)
+
+        # compute classification loss
+        cls_loss = self.get_class_prediction_loss(pred_cls, target_cls, has_object_map) * inv_n
+
+        # compute no-object loss
+        no_obj_loss = self.get_no_object_loss(pred_boxes_list, has_object_map) * inv_n
+
+        # Re-shape boxes in pred_boxes_list and target_boxes to meet the following desires
+        # 1) only keep having-object cells
+        # 2) vectorize all dimensions except for the last one for faster computation
+        pred_boxes_list = [pred_boxes[has_object_map] for pred_boxes in pred_boxes_list]
+        target_boxes = target_boxes[has_object_map].view(-1, 4)
+
+        # compute regression loss between the found best bbox and GT bbox for all the cell containing objects
+        best_ious, best_boxes = self.find_best_iou_boxes(pred_boxes_list, target_boxes)
+
+        # compute contain_object_loss
+        containing_obj_loss = self.get_contain_conf_loss(best_boxes[..., 4].unsqueeze(-1),
+                                                         torch.ones_like(best_ious)) * inv_n
 
         # compute regression loss
         reg_loss = self.get_regression_loss(best_boxes[..., :4], target_boxes[..., :4]) * inv_n
